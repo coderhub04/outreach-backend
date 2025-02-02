6619eedcd4a9f05d11222bd8a73eeb7270729d67
mrudulkolambe <mrudulkolambe02@gmail.com>
2025-02-02 20:13:20 +0530

    updated document

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -244,7 +244,7 @@ const getCurrentProfileController = async (req, res) => {
         const followingCount = await FollowingModel.countDocuments({
             userId: user._id
         })
-        const feedCount = await FeedsModel.feedCount({userId: user._id, deleted: false, block: false})
+        const feedCount = await FeedsModel.countDocuments({userId: user._id, deleted: false, block: false})
         const userResponse = {
             ...user.toObject(),
             feeds,
